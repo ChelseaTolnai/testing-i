1dbd6441eb3b9ed71809a2d324776163417f6eb2
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-06 13:46:51 -0500

    Additional item key value params set.

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -6,12 +6,12 @@ module.exports = {
 function objCheck(item) {
     if (typeof item === 'object') {
         const objCheck =  Object.prototype.toString.call(item).slice(8, -1);
-        if (objCheck === 'Object' ){
+        if (objCheck === 'Object' ) {
             let { name, type, durability, enhancement } = item
             if (typeof name === 'string' 
              && typeof type === 'string' && (type === 'armor' || type === 'weapon')
-             && typeof durability === 'number' 
-             && typeof enhancement === 'number'
+             && typeof durability === 'number' && (durability <= 100)
+             && typeof enhancement === 'number' && (0 <= enhancement && enhancement <= 20)
             ) {
                 return true
             }
@@ -31,8 +31,10 @@ function repair(item) {
 };
 
 function success(item) {
-    if (objCheck(item) === true){
-        return true
+    if (objCheck(item) === true) {
+        return {
+        ...item,
+        }
     }
     return null
 }
--- enhancer/enhancer.test.js
+++ enhancer/enhancer.test.js
@@ -21,6 +21,25 @@ describe('enhancer.js', () => {
         })
     })
 
+    describe('repair()', () => {
+        it('repairs item only if item is true object with required key value pairs', () => {
+            const expected = {
+                name: 'name',
+                type: 'armor',
+                durability: 100,
+                enhancement: 0,                
+            }
+            expect(repair(item)).toEqual(expected);
+            expect(repair(item2)).toBeNull();
+            expect(repair(item3)).toBeNull();
+            expect(repair()).toBeNull();
+            expect(repair(['array'])).toBeNull();
+            expect(repair(NaN)).toBeNull();
+            expect(repair(undefined)).toBeNull();
+            expect(repair(null)).toBeNull();
+        })
+    })
+
 })
 
 
